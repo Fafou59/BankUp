ad64d3269f3c268145b0504d3fad57665bb65987
lisamossion <45540040+lisamossion@users.noreply.github.com>
2018-12-04 20:04:03 +0100

    Add files via upload

--- menu.php
+++ menu.php
@@ -10,13 +10,16 @@
 
     <header>
         <nav>
-            <div id="item_Menu"><a href="index.php" title="Accueil"><img id="logo_BankUP" src="logo.png" alt="Accueil" /></a></div>
-            <div id="item_Menu"><a href="index.php" title="Accueil"><i class="fa fa-home"></i> Accueil</a></div>
+            <div id="item_Menu"></i> </a></div>
+            <div id="item_Menu"><a href="index.php" title="Accueil"><img id="logo_BankUP" src="logo.png" onclick="location.href='index.php'"alt="Accueil" /></a></div>
+            <div id="item_Menu"><a href="index.php" title="Accueil"> </a></div>
+            <div id="item_Menu"></i> </a></div>
+            <div id="item_Menu"></i> </a></div>
             <?php if ($_SESSION['connexion']=='') { ?>
-                <div id="item_Menu"><a href="connexion.php" title="Se connecter"> Se connecter</a></div>
-                <div id="item_Menu"><a href="inscription.php" title="Ouvrir un compte">Ouvrir un compte</a></div>
+                <div id="item_Menu"><button type="button" onclick="location.href='connexion.php'" title="Connexion" class="bouton_Connexion">CONNEXION</button></div>
+                <div id="item_Menu"><button type="button" onclick="location.href='inscription.php'" title="Inscription" class="bouton_Inscription">DEVENIR CLIENT</button></div>
             <?php } else {?>
-                <div id="item_Menu"><a href="#" title="Espace client"><i class="fa fa-lock"> Espace client</a></div>
+                <div id="item_Menu"><a href="#" title="Espace client">Espace client</a></div>
                 <div id="item_Menu"><a href="#" title="Se déconnecter">Se déconnecter</a></div>
             <?php } ?>
         </nav>
